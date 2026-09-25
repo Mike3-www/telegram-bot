afbf14825648b6c12ac33e8d48ca8b063caf6094
Mike3-www <96323131+Mike3-www@users.noreply.github.com>
2026-09-24 20:52:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ import json
 import time
 from flask import Flask, request, jsonify
 
-BOT_TOKEN = os.environ.get(8894073430:AAHQ9XxP0CpGa-qZrsLQZZywAbL1GAca6s8, "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 print(f"DEBUG_BOT_TOKEN: [{BOT_TOKEN}]")
-MY_CHAT_ID = os.environ.get(1685205799, "")
-WEBHOOK_URL = os.environ.get(https://telegram-bot-bpcp.onrender.com/webhook, "")
+MY_CHAT_ID = os.environ.get("MY_CHAT_ID", "")
+WEBHOOK_URL = os.environ.get("WEBHOOK_URL", "")
+
+bot_url = f"https://api.telegram.org/bot{BOT_TOKEN}" if BOT_TOKEN else ""
+
+app = Flask('')
+
 
 bot_url = f"https://api.telegram.org/bot{BOT_TOKEN}" if BOT_TOKEN else ""
 
